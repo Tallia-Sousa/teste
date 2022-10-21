d8c3ad56ba11cc76d8da63ded92cafc2b2831fe1
HILIZANGELA TALLIA <105807771+Tallia-Sousa@users.noreply.github.com>
2022-10-21 18:29:57 -0300

    Add files via upload

---
+++ ContaPoupanca.java
@@ -0,0 +1,24 @@
+package com.br;
+
+public  class ContaPoupanca extends Conta {
+
+    private double rendimento;
+
+    public ContaPoupanca(Cliente Cliente) {
+        super(Cliente);
+
+
+    }
+
+    public void setRendimento(double rendimento) {
+        this.rendimento = rendimento;
+    }
+
+    public double getRendimento() {
+        return rendimento;
+    }
+    //public double getSaldoDISponivel(){
+        //return super
+
+  //  }
+}
