d9f5d6d479e0aec0b4be075b91cfb1145c213b61
HILIZANGELA TALLIA <105807771+Tallia-Sousa@users.noreply.github.com>
2023-01-04 16:35:07 -0300

    Add files via upload

---
+++ RegistroServico.java
@@ -0,0 +1,15 @@
+package Salao.Beleza;
+
+import java.time.LocalDate;
+
+public class RegistroServico implements RegistroServicos {
+    private Servico servico;
+    private Cliente cliente;
+    private Funcionario funcionario;
+
+
+    @Override
+    public void registroServicos(String nomefuncionario, String nomeservico, String nomecliente, LocalDate dataServico) {
+
+    }
+}
