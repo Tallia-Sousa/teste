a5d635e74792389006ac8e1d5c8d083e80724ef9
HILIZANGELA TALLIA <105807771+Tallia-Sousa@users.noreply.github.com>
2023-01-04 16:37:15 -0300

    Add files via upload

---
+++ RegistroServicos.java
@@ -0,0 +1,10 @@
+package Salao.Beleza;
+
+import java.time.LocalDate;
+
+public interface RegistroServicos {
+
+
+    public void registroServicos(String nomefuncionario, String nomeservico, String nomecliente, LocalDate dataServico);
+
+}
